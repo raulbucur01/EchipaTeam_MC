18f25a0f7d7abb50bce803908b44e2e4cb40646e
leonardFlorin31 <leonard.beciu@student.unitbv.ro>
2023-10-26 13:13:47 +0300

    implementari metode clasa Player2 (2)

--- Project1/Player2.cpp
+++ Project1/Player2.cpp
@@ -10,3 +10,18 @@ std::string Player2::GetName() const
     return m_name;
 }
 
+int16_t Player2::GetScore() const
+{
+    return m_score;
+}
+
+int16_t Player2::SetScore(int16_t score)
+{
+     m_score = score;
+}
+
+std::string Player2::SetName(const std::string& name)
+{
+    m_name = name;
+}
+
