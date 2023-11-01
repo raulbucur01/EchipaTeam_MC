d55b9e54af4c9acb135a4b42adf0279178765311
raulbucur01 <raul.bucur@student.unitbv.ro>
2023-11-01 19:23:22 +0200

    Clasa word iter 2

--- SkribblFake/Word.cpp
+++ SkribblFake/Word.cpp
@@ -3,9 +3,51 @@
 Word::Word(std::string word)
 	: m_word(word)
 {
+	m_slots = getEmptyLetterSlots();
 }
 
 std::string Word::getWord()
 {
 	return m_word;
+}
+
+std::string Word::getCurrentSlotState()
+{
+	return m_slots;
+}
+
+void Word::revealOneRandomLetter()
+{
+	std::vector<int> unrevealedIndexes;
+	for (int i = 0; i < m_slots.length(); i++)
+	{
+		unrevealedIndexes.push_back(i);
+	}
+
+	//if (unrevealedIndexes.empty())
+	//{
+	//	return m_slots;
+	//}
+
+	int randomIndex = 1 + (rand() % m_word.length());
+	//if (randomIndex < 0 || randomIndex > m_word.length())
+	//	return m_slots;
+
+	m_slots[randomIndex] = m_word[randomIndex];
+	//return m_slots;
+}
+
+std::string Word::getEmptyLetterSlots()
+{
+	std::string letterSlots = "";
+	for (int i = 0; i < m_word.length(); i++)
+	{
+		letterSlots += "_";
+	}
+	return letterSlots;
+}
+
+void Word::resetSlots()
+{
+	m_slots = getEmptyLetterSlots();
 }
--- SkribblFake/Word.h
+++ SkribblFake/Word.h
@@ -1,14 +1,24 @@
 #pragma once
 #include <string>
+#include <vector>
+// Raul
 class Word
 {
 private:
 	std::string m_word;
+	std::string m_slots;
 
 public:
 	Word(std::string word = "");
+	// returneaza cuvantul
 	std::string getWord();
-	std::string revealOneRandomLetter();
-	std::string getLetterSlots();
+	// returneaza starea curenta a sloturilor de litere
+	std::string getCurrentSlotState();
+	// inlocuieste random un slot cu litera de la acelasi index din cuvant
+	void revealOneRandomLetter();
+	// returneaza un string cu atatea sloturi cate litere are cuvantul
+	std::string getEmptyLetterSlots();
+	
+	void resetSlots();
 };
 
