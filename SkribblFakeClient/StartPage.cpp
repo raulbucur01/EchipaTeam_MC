0d097b77be39db8a5e9cef3512cdaa48fb871582
VladBastina <vlad.bastina@student.unitbv.ro>
2023-11-26 21:59:35 +0200

    start page design

--- SkribblFakeClient/StartPage.cpp
+++ SkribblFakeClient/StartPage.cpp
@@ -1,10 +1,21 @@
 #include "StartPage.h"
+#include <QDesktopWidget>
 
 StartPage::StartPage(QWidget* parent)
 	: QMainWindow(parent), loginPage(nullptr)
 {
 	ui.setupUi(this);
+	this->setStyleSheet("QMainWindow {background: 'lightblue';}");
+    QDesktopWidget* desktop = QApplication::desktop();
+    this->resize(desktop.size());
+    QPixmap bkgnd("/cutepuppy.jpg");
+    bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Window, bkgnd);
+    this->setPalette(palette);
 	connect(ui.pushButton_START, &QPushButton::pressed, this, &StartPage::on_pushButton_Start_pressed);
+    ui.pushButton_START->setStyleSheet(QString("#%1 { background-color: lightgreen; }").arg(ui.pushButton_START->objectName()));
+    ui.pushButton_START->move(this->size().width()/2,this->size().height()/2);
 }
 
 StartPage::~StartPage()
