3c3e46b6c0504ba235b6460bb9dcb75592fcaff4
VladBastina <vlad.bastina@student.unitbv.ro>
2023-12-03 22:51:26 +0200

    Revert "buton iesire start page"
    This reverts commit 6e64377027437cb3b989a3e747f82151459956dc.

--- SkribblFakeClient/StartPage.cpp
+++ SkribblFakeClient/StartPage.cpp
@@ -16,22 +16,12 @@ StartPage::StartPage(QWidget* parent)
     ui.pushButton_START->setStyleSheet(QString("#%1 { background-color: lightgreen; }").arg(ui.pushButton_START->objectName()));
     ui.pushButton_START->move((this->size().width()-ui.pushButton_START->size().width())/2,(this->size().height() - ui.pushButton_START->size().height())/2);
     connect(ui.pushButton_START, &QPushButton::pressed, this, &StartPage::on_pushButton_Start_pressed);
-    ui.pushButton_START->setStyleSheet(QString("#%1 { background-color: red; }").arg(ui.pushButton_START->objectName()));
-    ui.pushButton_START->move((this->size().width() - ui.pushButton_START->size().width()), this->size().height());
-    connect(ui.exitButton, &QPushButton::pressed, this, &StartPage::on_exitButton_pressed);
 
 }
 
 StartPage::~StartPage()
 {
 }
-
-void StartPage::on_exitButton_pressed()
-{
-    return;
-}
-
-
 
 void StartPage::on_pushButton_Start_pressed() {
 	ui.pushButton_START->hide();
