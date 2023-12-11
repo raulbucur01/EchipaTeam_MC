d46c2d6071222e256999d74fbffec6e25de018e6
raulbucur01 <raul.bucur@student.unitbv.ro>
2023-12-11 13:07:09 +0200

    Decomentat partile problema din Client

--- SkribblFakeClient/RegisterPage.cpp
+++ SkribblFakeClient/RegisterPage.cpp
@@ -2,6 +2,7 @@
 #include <qmessagebox.h>
 #include <QScreen>
 #include "LoginPage.h"
+#include <cpr/cpr.h>
 
 RegisterPage::RegisterPage(QWidget *parent)
 	: QWidget(parent)
@@ -38,23 +39,23 @@ void RegisterPage::on_pushButton_CreateAccount_pressed() {
 
 	// dupa adaugare se trece la log in iar
 
-	/*auto res = cpr::Put(cpr::Url{"http://localhost:18080/signup"});
+	auto res = cpr::Put(cpr::Url{"http://localhost:18080/signup"});
 	if (res.error.code != cpr::ErrorCode::OK)
 	{
 		// error
 		return;
 	}
 	if (res.status_code == 201)
-	{*/
+	{
 		ui.groupBox_Register->hide();
 		ui.exitButton->hide();
 		delete ui.groupBox_Register;
 		delete ui.exitButton;
 		LoginPage* loginPage = new LoginPage(this);
 		loginPage->show();
-	/*}
-	else
+	}
+	/*else
 	{
-		//return error
+		return error;
 	}*/
 	}
