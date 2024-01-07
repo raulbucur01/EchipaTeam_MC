67f86634c84a5f16f5f1f60e9cc05dc9f9fc663a
raulbucur01 <raul.bucur@student.unitbv.ro>
2024-01-07 17:06:34 +0200

    .

--- SkribblFakeClient/ShopPage.cpp
+++ SkribblFakeClient/ShopPage.cpp
@@ -4,15 +4,45 @@ ShopPage::ShopPage(QWidget *parent, Player player)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
-	QPixmap pix("C:/MY_CODE/GitHubRepos/EchipaTeam_MC/SkribblFakeClient/Icons/Siren.jpeg");
-	QIcon icon(pix);
+	QPixmap pix1("C:/MY_CODE/GitHubRepos/EchipaTeam_MC/SkribblFakeClient/Icons/Siren.jpeg");
+	QIcon icon1(pix1);
 
     // Set the icon on the button
-    ui.pushButton_Icon1->setIcon(icon);
+    ui.pushButton_Icon1->setIcon(icon1);
 
     // Set the size of the icon to cover the entire button
-    QSize iconSize(ui.pushButton_Icon1->size());
-    ui.pushButton_Icon1->setIconSize(iconSize);
+    QSize iconSize1(ui.pushButton_Icon1->size());
+    ui.pushButton_Icon1->setIconSize(iconSize1);
+
+    QPixmap pix2("C:/MY_CODE/GitHubRepos/EchipaTeam_MC/SkribblFakeClient/Icons/Gladiator.jpeg");
+    QIcon icon2(pix2);
+
+    // Set the icon on the button
+    ui.pushButton_Icon2->setIcon(icon2);
+
+    // Set the size of the icon to cover the entire button
+    QSize iconSize2(ui.pushButton_Icon2->size());
+    ui.pushButton_Icon2->setIconSize(iconSize2);
+
+    QPixmap pix3("C:/MY_CODE/GitHubRepos/EchipaTeam_MC/SkribblFakeClient/Icons/Pig.jpeg");
+    QIcon icon3(pix3);
+
+    // Set the icon on the button
+    ui.pushButton_Icon3->setIcon(icon3);
+
+    // Set the size of the icon to cover the entire button
+    QSize iconSize3(ui.pushButton_Icon3->size());
+    ui.pushButton_Icon3->setIconSize(iconSize3);
+
+    QPixmap pix4("C:/MY_CODE/GitHubRepos/EchipaTeam_MC/SkribblFakeClient/Icons/Wizard.jpeg");
+    QIcon icon4(pix4);
+
+    // Set the icon on the button
+    ui.pushButton_Icon4->setIcon(icon4);
+
+    // Set the size of the icon to cover the entire button
+    QSize iconSize4(ui.pushButton_Icon4->size());
+    ui.pushButton_Icon4->setIconSize(iconSize4);
 }
 
 ShopPage::~ShopPage()
